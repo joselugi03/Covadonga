1cbf357a14504c5beda8eda2e696944175e32948
joselugi03 <jose150973@gmail.com>
2017-03-30 07:26:44 +0200

    Add files via upload

--- covadonga.php
+++ covadonga.php
@@ -15,32 +15,54 @@
             </div><br/>
             <div id="precuerpo">
 			<?php
-			if(isset($_POST["edad"]))
-				$edad = $_POST["edad"];
-			
+			if (isset($_POST["edad"]))
+				$edad=$_POST["edad"];
 			$valoracion=$_POST["valoracion"];
 			if (isset($_POST["sexo"]))
 				$sexo=$_POST["sexo"];
 			if(isset($_POST["gustos"])){
-			$gustos=$_REQUEST["gustos"];}
-		
-			$mensaje=$_POST["mensaje"];
+				$gustos=$_REQUEST["gustos"];}
+			if (isset($_POST["mensaje"]))
+				$mensaje=$_POST["mensaje"];
 			echo "<h2>Estos son los datos que nos ha proporcionado en tu formulario</h2>";
             echo "<table>";
-            if (isset($edad))
+            if (!empty("$edad"))
             {
-            	echo "<tr>";
-				echo "<td>Edad: $edad</td>";
-				echo "</tr>";
+            	if ($edad>0 && $edad<120)
+            	{
+            		echo "<tr>";
+					echo "<td><strong>Edad: </strong></td><td>$edad&nbsp;&nbsp;</td>";
+					echo "</tr>";
+            	}else 
+            	{
+            		echo "<tr>";
+            		echo "<td><strong>Edad: </strong></td><td>La edad introducida no se ajusta a los parametros</td>";
+            		echo "</tr>";
+            	}
+            		
             }
+            if (!empty($sexo))
+            	echo "<tr>";
+            	echo "<td><strong>Sexo: </strong></td><td>$sexo&nbsp;&nbsp;</td>";
+            	echo "</tr>";
             echo "<tr>";
-            echo "<td>Valoracion: $valoracion</td>";
+            echo "<td><strong>Valoracion: </strong></td><td> $valoracion</td>";
             echo "</tr>";
-			foreach ($gustos as $gusto)
-			{	
-				echo "<tr><td>Tegustaría: $gusto </td></tr> ";
-			}
-			echo "<div>Este ha sido tu comentario: $mensaje</div>";
+            if (!empty($gustos))
+            {
+				foreach ($gustos as $gusto)
+				{	
+					if ($gusto!="otro concepto" && $gusto!="noVolver")
+						echo "<tr><td><strong>Te gustarían más: </strong></td><td>$gusto &nbsp;</td></tr> ";
+					else 
+						echo "<tr><td><strong>Te gustaría más: </strong></td><td>$gusto &nbsp;</td></tr> ";
+				}
+            }
+            if (!empty($mensaje)){
+            	echo "<tr><td colspan=2><strong>Este ha sido tu comentario:</strong></td></tr>
+            	<tr><td colspan=2>$mensaje</td></tr>";
+            }
+			
 			echo "</table>";
 			?>
 			<div>
